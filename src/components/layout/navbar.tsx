25a0f567cfd8a1d5194c3fa82e13d4d49eb1f29a
muhamadafwan <muhamad_afwan@teknokrat.ac.id>
2026-06-03 18:34:14 +0700

    membuat tampilan panduan

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
             </Link>
 
             <Link
-              href="#"
+              href="/panduan"
               className="text-sm font-medium text-white/70 no-underline transition hover:text-white"
             >
               Panduan
@@ -123,7 +123,7 @@ export default function Navbar() {
               </Link>
 
               <Link
-                href="#"
+                href="/panduan"
                 className="text-sm font-semibold text-white/70 no-underline transition hover:text-white"
                 onClick={() => setIsOpen(false)}
               >
